09c6b16bb0dfe3d0cc44eae68576817d76bdb711
saliq03 <javidsaliq@gmail.com>
2024-11-03 13:50:45 +0530

    setting time show in messages

--- lib/view/pages/chat_screen/widgets/message_tile.dart
+++ lib/view/pages/chat_screen/widgets/message_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:unishare/model/user_model/user_model.dart';
-import 'package:unishare/viewmodels/user_prefrences/user_prefrences.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../../../model/message_model/message.dart';
 
@@ -10,15 +10,38 @@ class MessageTile extends StatelessWidget {
   final Message newMessage;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: currentUser==newMessage.senderId?
-        CrossAxisAlignment.end:
-        CrossAxisAlignment.start,
-        children: [
-          Text(newMessage.message),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: currentUser == newMessage.senderId
+          ? CrossAxisAlignment.end
+          : CrossAxisAlignment.start,
+      children: [
+        Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 16, top: 10,right: 50,bottom: 25),
+              margin: EdgeInsets.symmetric(vertical: 2.5, horizontal: 25),
+              constraints: BoxConstraints(
+                maxWidth: 300.0, // Maximum width for the message container
+              ),
+              decoration: BoxDecoration(
+                color: currentUser == newMessage.senderId ? Colors.green : Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(14),
+              ),
+              child: Text(
+                newMessage.message,
+                softWrap: true, // Allow the text to wrap
+                maxLines: null, // Allow unlimited lines
+                style: TextStyle(fontSize: 16), // Optional: set text style
+              ),
+            ),
+            Positioned(
+                right: 33,bottom: 7,
+                child: Text(DateFormat.jm().format(DateTime.timestamp()),style: TextStyle(fontSize: 13,color: Colors.black45),))
+          ],
+        ),
+      ],
     );
+
+
   }
 }
